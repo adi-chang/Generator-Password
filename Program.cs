3c6bd1167ddcb3a5e0ee5f416a8b75845627e163
Adi <adi4vista@gmail.com>
2023-12-13 13:35:25 +0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿/// sample project 
+/// Password Generator 
+/// using helper library from : https://www.nuget.org/packages/PasswordGenerator/
+
+using PasswordGenerator;
+
+// var pwd = new Password();
+
+IPassword? pwd = 
+   new Password(8) // default, it will generate 16 karakter 
+   .IncludeUppercase()
+   .IncludeLowercase()
+   .IncludeNumeric()
+   .IncludeSpecial();
+
+Enumerable.Range(1, 10).ToList().ForEach(i => {
+   var password = pwd.Next();
+   Console.WriteLine($"{i, 2}. Password Generated = {password}");
+});
